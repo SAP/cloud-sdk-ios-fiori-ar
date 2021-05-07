f13509c8e1d3d7e4af5ecb893bd42f48bab777d7
pgobriensap <77754056+pgobriensap@users.noreply.github.com>
2021-05-07 01:35:05 -0500

    Fixed Reuse Headers (#4)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,6 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// SPDX-FileCopyrightText: 2021 2020 SAP SE or an SAP affiliate company and cloud-sdk-ios-fioriarkit contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-
 import PackageDescription
 
 let package = Package(
