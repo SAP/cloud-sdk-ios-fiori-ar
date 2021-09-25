cfcb4084b5406e15c7407ad966ba1b7be44db60b
Pat <p.o-brien@sap.com>
2021-09-24 20:56:44 -0500

    feat: 🎸 Improved Card Creation Actions
    (cherry picked from commit 4f738d3bc83a0af78084c34594d527f44f036e5a)

--- Sources/FioriARKit/ARCards/RealityKit/LoadingStrategies/AnnotationLoadingStrategy.swift
+++ Sources/FioriARKit/ARCards/RealityKit/LoadingStrategies/AnnotationLoadingStrategy.swift
@@ -1,24 +1,12 @@
 //
-//  ARContainer.swift
-//  ARTestApp
+//  AnnotationLoadingStrategy.swift
 //
-//  Created by O'Brien, Patrick on 1/20/21.
+//
+//  Created by O'Brien, Patrick on 9/24/21.
 //
 
 import ARKit
-import Combine
 import RealityKit
-import SwiftUI
-
-internal struct ARContainer: UIViewRepresentable {
-    var arStorage: ARManager
-    
-    func makeUIView(context: Context) -> ARView {
-        self.arStorage.arView ?? ARView(frame: .zero)
-    }
-
-    func updateUIView(_ arView: ARView, context: Context) {}
-}
 
 /// Protocol which defines the data a strategy needs to provide a `[ScreenAnnotation]`
 public protocol AnnotationLoadingStrategy {
@@ -60,5 +48,3 @@ extension SceneLoadable {
         return annotations
     }
 }
-
-typealias AnchorID = UUID
