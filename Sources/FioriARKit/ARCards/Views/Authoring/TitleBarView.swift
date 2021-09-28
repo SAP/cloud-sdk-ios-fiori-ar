4fa4d50c26798131d6d8b397f3381bef2c5fb6b3
Pat <p.o-brien@sap.com>
2021-09-27 22:06:47 -0500

    feat: 🎸 AuthoringFlow
    (cherry picked from commit 98886d5e08b5cfd573f69a2deb4013f34fdcf35c)

--- Sources/FioriARKit/ARCards/Views/Authoring/TitleBarView.swift
+++ Sources/FioriARKit/ARCards/Views/Authoring/TitleBarView.swift
@@ -30,18 +30,23 @@ struct TitleBarView<LeftBarLabel, RightBarLabel>: View where LeftBarLabel: View,
     
     var body: some View {
         HStack {
-            Button(action: { onLeftAction?() }, label: {
-                leftBarLabel()
-            })
-                .padding(.leading, 16)
-            Spacer()
+            HStack {
+                Button(action: { onLeftAction?() }, label: {
+                    leftBarLabel()
+                })
+                Spacer()
+            }
+            
             Text(title).bold()
-            Spacer()
-            Button(action: { onRightAction?() }, label: {
-                rightBarLabel()
-            })
-                .padding(.trailing, 16)
+            
+            HStack {
+                Spacer()
+                Button(action: { onRightAction?() }, label: {
+                    rightBarLabel()
+                })
+            }
         }
+        .padding(.horizontal, 16)
         .frame(height: 52)
         .padding(.top, 44)
     }
