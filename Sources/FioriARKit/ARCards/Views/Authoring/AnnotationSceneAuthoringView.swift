635f75a5fcd32c6c0fe8829566f49e992e9dff4f
Pat <p.o-brien@sap.com>
2021-10-01 00:50:57 -0500

    refactor: 💡 Refactoring Views And Layouts

--- Sources/FioriARKit/ARCards/Views/Authoring/AnnotationSceneAuthoringView.swift
+++ Sources/FioriARKit/ARCards/Views/Authoring/AnnotationSceneAuthoringView.swift
@@ -1,5 +1,5 @@
 //
-//  CardAuthoringView.swift
+//  AnnotationSceneAuthoringView.swift
 //
 //
 //  Created by O'Brien, Patrick on 9/13/21.
@@ -8,6 +8,7 @@
 import SwiftUI
 
 public struct AnnotationSceneAuthoringView: View {
+    @Environment(\.verticalSizeClass) var vSizeClass
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.onCardEdit) var onCardEdit
     
@@ -15,7 +16,7 @@ public struct AnnotationSceneAuthoringView: View {
     
     @State private var currentTab: TabSelection
     
-    @State private var anchorImage: Image?
+    @State private var anchorImage: UIImage?
     @State private var cardItems: [DecodableCardItem]
     @State private var attachmentItemModels: [AttachmentItemModel]
     @State private var currentCardID: UUID?
@@ -50,50 +51,57 @@ public struct AnnotationSceneAuthoringView: View {
             })
                 .background(Color.white)
             
-            TabView(currentTab: $currentTab, leftTabTitle: "Cards", rightTabTitle: "Anchor Image")
-            
-            switch currentTab {
-            case .left:
-                AttachementsView(attachmentItemModels: $attachmentItemModels, onAddAttachment: { cardCreationIsPresented.toggle() }) { attachmentItemModel in
-                    currentCardID = attachmentItemModel.id
-                    cardCreationIsPresented.toggle()
+            VStack(spacing: 0) {
+                TabView(currentTab: $currentTab, leftTabTitle: "Cards", rightTabTitle: "Anchor Image")
+                
+                switch currentTab {
+                case .left:
+                    AttachementsView(label: "Cards", attachmentItemModels: $attachmentItemModels,
+                                     onAddAttachment: { cardCreationIsPresented.toggle() }) { attachmentItemModel in
+                        currentCardID = attachmentItemModel.id
+                        cardCreationIsPresented.toggle()
+                    }
+                case .right:
+                    UploadAnchorImageTabView(anchorImage: $anchorImage)
+                    Spacer()
                 }
-            case .right:
-                UploadAnchorImageTabView(anchorImage: $anchorImage)
             }
-            Spacer()
+            .padding(.horizontal, vSizeClass == .compact ? 40 : 0)
+            .background(Color.white)
         }
         .background(
             NavigationLink(destination:
-                CardCreationView(cardItems: $cardItems,
-                                 attachmentModels: $attachmentItemModels,
-                                 currentCardID: $currentCardID)
+                CardFormView(cardItems: $cardItems,
+                             attachmentModels: $attachmentItemModels,
+                             currentCardID: $currentCardID)
                     .onCardEdit(perform: onCardEdit),
-                           
                 isActive: $cardCreationIsPresented,
                 label: { EmptyView() })
         )
         .navigationBarHidden(hideNavBar)
         .navigationBarTitle("")
-        .edgesIgnoringSafeArea(.top)
+        .edgesIgnoringSafeArea(.all)
         .onAppear(perform: populateAttachmentView)
     }
     
     func populateAttachmentView() {
+        self.attachmentItemModels.removeAll()
         self.cardItems.forEach { card in
-            if !attachmentItemModels.contains(where: { UUID(uuidString: card.id) == $0.id }) {
-                let newAttachmentModel = AttachmentItemModel(id: UUID(uuidString: card.id) ?? UUID(),
-                                                             title: card.title_,
-                                                             subtitle: card.position_ == nil ? "Not Pinned Yet" : "Pinned",
-                                                             info: nil,
-                                                             image: card.detailImage_,
-                                                             icon: card.icon_)
-                attachmentItemModels.append(newAttachmentModel)
-            }
+            let newAttachmentModel = AttachmentItemModel(id: UUID(uuidString: card.id) ?? UUID(),
+                                                         title: card.title_,
+                                                         subtitle: card.position_ == nil ? "Not Pinned Yet" : "Pinned",
+                                                         info: nil,
+                                                         image: card.detailImage_,
+                                                         icon: card.icon_)
+            attachmentItemModels.append(newAttachmentModel)
         }
     }
     
-    func startAR() {}
+    func startAR() {
+        if self.anchorImage == nil || self.cardItems.isEmpty {
+            print("TODO")
+        }
+    }
 }
 
 private struct TabView: View {
@@ -120,10 +128,10 @@ private struct TabView: View {
     func tab(title: String, isSelected: Bool) -> some View {
         VStack(spacing: 6) {
             Text(title)
-                .foregroundColor(isSelected ? Color.fnBlue : Color.black)
+                .foregroundColor(isSelected ? Color.fioriNextBlue : Color.black)
                 .bold()
             if isSelected {
-                Color.fnBlue.frame(height: 2)
+                Color.fioriNextBlue.frame(height: 2)
             } else {
                 Color.clear.frame(height: 2)
             }
