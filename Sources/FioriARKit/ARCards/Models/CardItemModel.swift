1f4b1c1e31d6c9949ec3199402a523c3aebaa51b
Marco Eidinger <marco.eidinger@sap.com>
2021-10-06 12:34:43 -0700

    WIP: create scene

--- Sources/FioriARKit/ARCards/Models/CardItemModel.swift
+++ Sources/FioriARKit/ARCards/Models/CardItemModel.swift
@@ -30,6 +30,15 @@ public protocol IconComponent {
 }
 
 public struct DecodableCardItem: CardItemModel {
+    public init(id: String, title_: String, descriptionText_: String? = nil, detailImage_: Image? = nil, actionText_: String? = nil, icon_: Image? = nil) {
+        self.id = id
+        self.title_ = title_
+        self.descriptionText_ = descriptionText_
+        self.detailImage_ = detailImage_
+        self.actionText_ = actionText_
+        self.icon_ = icon_
+    }
+
     public var id: String
     public var title_: String
     public var descriptionText_: String?
