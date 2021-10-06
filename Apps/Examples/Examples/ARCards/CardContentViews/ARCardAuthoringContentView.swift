840d60c2dc75cbfd6c5a55440e14a6046dfac671
Marco Eidinger <marco.eidinger@sap.com>
2021-10-06 15:12:55 -0700

    refactor: 💡 wip: e2e skeleton for card authoring
    create a scene and upload to server

--- Apps/Examples/Examples/ARCards/CardContentViews/ARCardAuthoringContentView.swift
+++ Apps/Examples/Examples/ARCards/CardContentViews/ARCardAuthoringContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import FioriARKit
+import SAPFoundation
 import SwiftUI
 
 struct ARCardAuthoringContentView: View {
@@ -13,9 +14,23 @@ struct ARCardAuthoringContentView: View {
                      DecodableCardItem(id: UUID().uuidString, title_: "World"),
                      DecodableCardItem(id: UUID().uuidString, title_: "Fizz"),
                      DecodableCardItem(id: UUID().uuidString, title_: "Buzz")]
-    
+
+    private var sapURLSession: SAPURLSession
+
+    init() {
+        sapURLSession = SAPURLSession()
+
+        // if user is not yet authenticated then webview will present IdP form
+        sapURLSession.attachOAuthObserver(
+            clientID: "d7977a0b-c0d3-474c-8d7c-dfd1e3e5245b",
+            authURL: "https://mobile-tenant1-xudong-iosarcards.cfapps.sap.hana.ondemand.com/oauth2/api/v1/authorize",
+            redirectURL: "https://mobile-tenant1-xudong-iosarcards.cfapps.sap.hana.ondemand.com",
+            tokenURL: "https://mobile-tenant1-xudong-iosarcards.cfapps.sap.hana.ondemand.com/oauth2/api/v1/token"
+        )
+    }
+
     var body: some View {
-        AnnotationSceneAuthoringView(cardItems)
+        AnnotationSceneAuthoringView(cardItems, sapURLSession: sapURLSession)
             .onCardEdit { cardEdit in
                 switch cardEdit {
                 case .created(let card):
