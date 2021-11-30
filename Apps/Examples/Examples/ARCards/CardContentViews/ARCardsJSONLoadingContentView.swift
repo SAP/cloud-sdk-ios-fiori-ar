5f021ac539ba1a03b9047a4f0ebb79453aac3203
Marco Eidinger <marco.eidinger@sap.com>
2021-11-30 15:26:36 -0800

    style: 💄 run swiftformat

--- Apps/Examples/Examples/ARCards/CardContentViews/ARCardsJSONLoadingContentView.swift
+++ Apps/Examples/Examples/ARCards/CardContentViews/ARCardsJSONLoadingContentView.swift
@@ -18,7 +18,7 @@ struct ARCardsJSONLoadingContentView: View {
                               // set the card action for id corresponding to the CardItemModel
                               print(id)
                           })
-                          .onAppear(perform: loadInitialData)
+            .onAppear(perform: loadInitialData)
     }
     
     func loadInitialData() {
